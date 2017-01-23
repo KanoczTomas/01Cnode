19f8e65d9a297856f90722622c61616f933ff3e8
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-01-23 14:25:37 +0000

    moved the api tests to a separate folder, making it more modular and easier to write

---
+++ tests/api/bitcoind/getinfo.js
@@ -0,0 +1,86 @@
+var should = require("should");
+var request = require("supertest");
+var config = require("config");
+
+describe('bitcoind rpc call restful API routing tests:', function(){
+var url = "http://localhost:" + config.get('Web.port');
+	describe('/api/bitcoind/getinfo tests:', function(done){
+		var route = "/api/bitcoind/getinfo";
+		it('should throw when incoret route given', function(done){
+			request(url)
+			.get("/something/bad")
+			.expect(200)
+			.end(function(err, res){
+				err.should.be.Error();
+				done();
+			});
+		});
+		it('should return a json object', function(done){
+			request(url)
+			.get(route)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res){
+				res.body.should.be.Object()
+				done();
+			});
+		});
+		it('attribute connections should not be null', function(done){
+			request(url)
+			.get(route)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res){
+				console.log(err);
+				res.body.connections.should.not.be.null();
+				done();
+			})
+		});
+		it('attribute difficulty should not be null', function(done){
+			request(url)
+			.get(route)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res){
+				res.body.difficulty.should.not.be.null();
+				done();
+			});
+		});
+		it('attribute version should not be null', function(done){
+			request(url)
+			.get(route)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res){
+				res.body.version.should.not.be.null();
+				done();
+			});
+		});
+		it('attribute blocks should not be null', function(done){
+			request(url)
+			.get(route)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res){
+				res.body.blocks.should.not.be.null();
+				done();
+			});
+		});
+		it('attribute protocolversion should not be null', function(done){
+			request(url)
+			.get(route)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res){
+				res.body.protocolversion.should.not.be.null()
+				done();
+			});
+		});
+	});
+});
