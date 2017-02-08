760a725ea6af7a7a46d67430a46fa43ec8753b9b
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-02-08 18:23:56 +0000

    promisified node-bitcoin-rpc in the api call factory file

--- js/api/bitcoind/index.js
+++ js/api/bitcoind/index.js
@@ -1,6 +1,8 @@
 var bitcoind = require("express").Router();
 var config = require("config");
+var Promise = require("bluebird");
 var bitcoinRPC = require("node-bitcoin-rpc");
+Promise.promisifyAll(bitcoinRPC);
 
 bitcoinRPC.init(config.get('RPC.host'), config.get('RPC.port'), config.get('RPC.rpc_username'), config.get('RPC.rpc_password'));
 
@@ -20,14 +22,12 @@ config.get('Api.restCalls').forEach(function(entry){
         if(entry.timeout){
             bitcoinRPC.setTimeout(entry.timeout);
         }
-                
-        bitcoinRPC.call(entry.callName, inputString, function (error, value) {
-           if(error) {
-               res.sendStatus(404).end();
-            }
-            else{
-                res.status(200).json(value.result).end();
-            }
+        bitcoinRPC.callAsync(entry.callName, inputString)
+        .then(function(value){
+            res.status(200).json(value.result).end();
+        })
+        .error(function(error){
+            res.sendStatus(404).end();
         });
     })
 });
