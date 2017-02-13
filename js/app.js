d9c375e26c0cd9a7141de0868f60e95a8d52bcff
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-02-13 22:44:44 +0100

    added loading-inline directive

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 require("angular");
 var config = require("config");
-require("./directives/loading")
+require("./directives/loading");
+require("./directives/loading-inline");
 
 
 var app = angular.module(config.get('Client.appName'),
   [ require("angular-ui-router"),
-    'directive.loading'
+    'directive.loading',
+    'directive.loading-inline'
   ]
 );
 app.config(require("./states"));
---
+++ js/directives/loading-inline.js
@@ -0,0 +1,26 @@
+var angular = require("angular");
+
+var app = angular.module("directive.loading-inline",[]);
+
+app.directive('loading-inline',   ['$http' ,function ($http)
+{
+    return {
+        restrict: 'A',
+        link: function (scope, elm, attrs) {
+            
+            scope.isLoading = function () {
+                return $http.pendingRequests.length > 0;
+            };
+            scope.$watch(scope.isLoading, function (v) {
+                if (v) {
+                    elm.show();
+                }
+                else {
+                    elm.hide();
+                }
+            });
+        },
+        template: "<span class=\"loader-dots text-center\">Loading <span>.</span><span>.</span><span>.</span></span>"
+    };
+}]);
+module.exports = app;
