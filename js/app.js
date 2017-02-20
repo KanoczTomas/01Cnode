0076b81663514d7783888da75bb9910d5d9bef7b
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-02-20 18:34:34 +0000

    added pageName config to default.yaml, added the status layout (using dl, dt,dd tags)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 require("angular");
 var config = require("config");
 require("./directives/loading");
+require("./controllers/indexCtrl");
+require("./filters/secondsToDateTime");
 
 
 var app = angular.module(config.get('Client.appName'),
   [ require("angular-ui-router"),
-    'directive.loading'
+    'directive.loading',
+    'controller.indexCtrl',
+    'filter.secondsToDateTime'
   ]
 );
 app.config(require("./states"));
