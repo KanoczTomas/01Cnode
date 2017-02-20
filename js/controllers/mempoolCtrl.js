b5b1edcdcb5720244fdcc67aaabc3c51f792bff6
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-02-20 18:49:00 +0000

    added timer to simulate the uptime is updated every seconds, beautified the mempoolCtrl

--- js/controllers/mempoolCtrl.js
+++ js/controllers/mempoolCtrl.js
@@ -1,22 +1,20 @@
-module.exports = [ '$scope', '$http', '$interval', function($scope, $http, $interval){
-    function loadMempool(){
+module.exports = ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+    function loadMempool() {
         //$http.get("/api/bitcoind/getrawmempool")
-        $http.get("/api/bitcoind/getmempoolinfo")
-        .then(function(res){
+        $http.get("/api/bitcoind/getmempoolinfo").then(function (res) {
             $scope.mempoolEntries = res.data;
         });
     }
     loadMempool();
-		if(!angular.isDefined(timer)){
-			var timer = $interval(function(){
-				loadMempool();
-			}, 2000);
-		}
-
-		$scope.$on("$destroy", function(){
-			if(angular.isDefined(timer)){
-				$interval.cancel(timer);
-				timer = undefined;
-			}
-		});
+    if (!angular.isDefined(timer)) {
+        var timer = $interval(function () {
+            loadMempool();
+        }, 2000);
+    }
+    $scope.$on("$destroy", function () {
+        if (angular.isDefined(timer)) {
+            $interval.cancel(timer);
+            timer = undefined;
+        }
+    });
 }];
--- js/controllers/overviewCtrl.js
+++ js/controllers/overviewCtrl.js
@@ -1,14 +1,23 @@
-module.exports = ['$scope', '$http', function($scope, $http){
-  
-  $scope.refresh = function(){
-      $http.get('/api/bitcoind/getpeerinfo')
-      .then(function(res){
-        $scope.peers = res.data;
-      });
-  };
-  $http.get('api/bitcoind/status')
-  .then(function(res){
-  	$scope.info = res.data;
-  });
-  $scope.refresh();
+module.exports = ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+    $scope.refresh = function () {
+        $http.get('/api/bitcoind/getpeerinfo').then(function (res) {
+            $scope.peers = res.data;
+        });
+    };
+    var timer;
+    $http.get('api/bitcoind/status').then(function (res) {
+        $scope.info = res.data;
+        if (!angular.isDefined(timer)) {
+            timer = $interval(function () {
+                $scope.info.uptime += 1; //we add 1 second to uptime to fake it is live
+            }, 1000);
+        }
+    });
+    $scope.refresh();
+    $scope.$on("$destroy", function () {
+        if (angular.isDefined(timer)) {
+            $interval.cancel(timer);
+            timer = undefined;
+        }
+    });
 }];
