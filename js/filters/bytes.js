cfbd59ff4bd610d8c55212d12c004f39376468c2
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-09-07 19:51:51 +0200

    added tests for filter as changed it to support more units bytes, bibytes and weight units, updated blocks view to show WUs

--- js/filters/bytes.js
+++ js/filters/bytes.js
@@ -5,26 +5,73 @@ var config = require("config");
 
 angular.module(config.get("Client.appName"))
 .filter('bytes', function() {
-	return function(bytes, unit, precision) {
+	return function filterBytes(bytes, unit, precision) {
 		if (bytes === 0 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
 		if (typeof precision === 'undefined') precision = 2;
-		var units = ['bytes', 'kBi', 'MBi', 'GBi', 'TBi', 'PBi'],
-			number = Math.floor(Math.log(bytes) / Math.log(1024));
-            
-        var h = (bytes / Math.pow(1024, Math.floor(number)))// 
+        var availableUnits = [
+            {
+                name: 'bibytes',
+                matchRegexp: /^bytes(\.dynamic)?$|^[kMGTP]Bi$/,//bytes.dynamic means it will find k M G T and so on automatically
+                units: ['bytes', 'kBi', 'MBi', 'GBi', 'TBi', 'PBi'],
+                base: 1024
+            },
+            {
+                name: 'bytes',
+                matchRegexp: /^SIbytes(\.dynamic)?$|^[kMGTP]B$/,
+                units: ['SIbytes', 'kB', 'MB', 'GB', 'TB', 'PB'],//I know SIbytes is ugly, but use bytes if you want to show bytes and not SIbytes as input to the filter
+                base: 1000
+            },
+            {
+                name: 'weight',
+                matchRegexp: /^WU(\.dynamic)?$|^[kMGTP]WU$/,
+                units: ['WU', 'kWU', 'MWU', 'GWU', 'TWU', 'PWU'],
+                base: 1000
+            },
+        ];
+        //we will parse the unit argument for any regexp match and set units variable to 
+        //abailableUnits.units
+        //first match is assigned! so order of definition matters
+        var base, units;
+        function setDefaults(){
+            var defaultUnit = availableUnits.filter(function (elm){
+                return elm.name === 'bibytes';
+            })[0];
+            base = defaultUnit.base;
+            units = defaultUnit.units;
+            filterBytes.unitName = defaultUnit.name;
+        }
+        if(typeof unit !== 'undefined'){
+            if(!availableUnits.some(function (availableUnit){
+                if(availableUnit.matchRegexp.test(unit)) {
+                    units = availableUnit.units;
+                    base = availableUnit.base;
+                    filterBytes.unitName = availableUnit.name;
+                    return true;//we return true so that setDefaults() does not kick in
+                }
+            })) setDefaults();//if unit is set, but does not match the availableUnits use defaults
+        }else {//we set the defaults in case unit is not given
+            setDefaults();
+        }
+        var number = Math.floor(Math.log(bytes) / Math.log(base));
+        
+        //we check for dynamics "property" - i test passes 1025 bytes will be 1.00 kBi or 1.025 kB ... and so on
+        if(/\.dynamic/.test(unit)) unit = undefined;
+    
+        var h = (bytes / Math.pow(base, Math.floor(number)))// 
         if(typeof unit !== 'undefined'){
             var unitIndex = units.indexOf(unit);
             if(unitIndex >= 0){
                 if(unitIndex > number){//e.g. unit = MBi, we have 1024 bytes = 1kBi, we need to return 0.00 MBi for precision 2 so we have to devide by 1024 * the distance between indexes
-                    h /= Math.pow(1024,unitIndex - number);
+                    h /= Math.pow(base,unitIndex - number);
                 }
                 else if(unitIndex < number){//in this case we have to multiply
-                    h *= Math.pow(1024, number - unitIndex);
+                    h *= Math.pow(base, number - unitIndex);
                 }
                 number = unitIndex;//we have to change the unit index
             } 
         }
         if(number === 0) precision = 0;//number is equal to 0 if we have bytes. We want 123 bytes, not 123.00
+        if(filterBytes.unitName === 'bytes') units[0] = 'bytes';//we must set the units to bytes instead of the ugly SIbytes
         return h.toFixed(precision) +  ' ' + units[number];;
 	}
 });
--- tests/js/filters/bytes.js
+++ tests/js/filters/bytes.js
@@ -34,12 +34,25 @@ describe('filter bytes', function(){
         filter(2000000000, undefined, 4).split(" ")[0].split(".")[1].length.should.be.equal(4);
     });
     it('should use the given unit if unit input variable set else return closest unit', function(){
+        //testing bibytes
         filter(1524,'test').split(" ")[1].should.be.equal('kBi');
         filter(1524,'MBi',4).should.be.equal("0.0015 MBi");
         filter(1524,'GBi',10).should.be.equal("0.0000014193 GBi");
         filter(16106127360).should.be.equal("15.00 GBi");
         filter(16106127360,'kBi').should.be.equal("15728640.00 kBi");
         filter(16106127360,'bytes').should.be.equal("16106127360 bytes");
+        //testing bytes
+        filter(1524,'MB',4).should.be.equal("0.0015 MB");
+        filter(1524,'GB',10).should.be.equal("0.0000015240 GB");
+        filter(16106127360, 'SIbytes.dynamic').should.be.equal("16.11 GB");
+        filter(16106127360,'kB').should.be.equal("16106127.36 kB");
+        filter(16106127360,'SIbytes').should.be.equal("16106127360 bytes");
+        //testing weight
+        filter(1524,'MWU',4).should.be.equal("0.0015 MWU");
+        filter(1524,'GWU',10).should.be.equal("0.0000015240 GWU");
+        filter(16106127360, 'WU.dynamic').should.be.equal("16.11 GWU");
+        filter(16106127360,'kWU').should.be.equal("16106127.36 kWU");
+        filter(16106127360,'WU').should.be.equal("16106127360 WU");
     });
     it('should return 1023 bytes for 1023', function(){
         filter(1023).should.be.equal("1023 bytes");
@@ -47,16 +60,46 @@ describe('filter bytes', function(){
     it('should return 1 kBi for 1024 bytes', function(){
         filter(1024).should.be.equal("1.00 kBi");
     });
+    it('should return 1 kB for 1000 bytes', function(){
+        filter(1000, 'SIbytes.dynamic').should.be.equal("1.00 kB");
+    });
+    it('should return 1 kWU for 1000 weight units', function(){
+        filter(1000, 'WU.dynamic').should.be.equal("1.00 kWU");
+    });
     it('should return 1 MBi for 1024*1024 bytes', function(){
         filter(1024*1024).should.be.equal("1.00 MBi");
+    });
+    it('should return 1 MB for 1000*1000 bytes', function(){
+        filter(1000*1000, 'SIbytes.dynamic').should.be.equal("1.00 MB");
+    });
+    it('should return 1 MWU for 1000*1000 weight units', function(){
+        filter(1000*1000, 'WU.dynamic').should.be.equal("1.00 MWU");
     });
     it('should return 1 GBi for 1024*1024*1024 bytes', function(){
         filter(1024*1024*1024).should.be.equal("1.00 GBi");
     });
+    it('should return 1 GB for 1000*1000*1000 bytes', function(){
+        filter(1000*1000*1000, 'SIbytes.dynamic').should.be.equal("1.00 GB");
+    });
+    it('should return 1 GWU for 1000*1000*1000 weight units', function(){
+        filter(1000*1000*1000, 'WU.dynamic').should.be.equal("1.00 GWU");
+    });
     it('should return 1 TBi for 1024*1024*1024*1024 bytes', function(){
         filter(1024*1024*1024*1024).should.be.equal("1.00 TBi");
     });
+    it('should return 1 TB for 1000*1000*1000*1000 bytes', function(){
+        filter(1000*1000*1000*1000, 'SIbytes.dynamic').should.be.equal("1.00 TB");
+    });
+    it('should return 1 TWU for 1000*1000*1000*1000 weight units', function(){
+        filter(1000*1000*1000*1000, 'WU.dynamic').should.be.equal("1.00 TWU");
+    });
     it('should return 1 PBi for 1024*1024*1024*1024*1024 bytes', function(){
         filter(1024*1024*1024*1024*1024).should.be.equal("1.00 PBi");
+    });
+    it('should return 1 PB for 1000*1000*1000*1000*1000 bytes', function(){
+        filter(1000*1000*1000*1000*1000, 'SIbytes.dynamic').should.be.equal("1.00 PB");
+    });
+    it('should return 1 PWU for 1000*1000*1000*1000*1000 weight units', function(){
+        filter(1000*1000*1000*1000*1000, 'WU.dynamic').should.be.equal("1.00 PWU");
     });
 });
